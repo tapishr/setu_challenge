c609393486bb930167a6b425631174948df43055
tapish <tapish303@gmail.com>
2020-06-27 16:57:34 +0530

    Add sample data

--- populate.py
+++ populate.py
@@ -1,29 +1,28 @@
+import json
 from app import db
 from models import Users
 from models import Customers
 from models import Transactions
 
 
-user = Users()
-user.user = "Setu"
-user.key = "f2550385427e404421b22c3c388f3a57"
-db.session.add(user)
-db.session.commit()
-
-
-cust = Customers()
-cust.name = "Tapish"
-cust.amount = 1323.0
-cust.duedate = "12-12-19"
-cust.mobileno = "9000090000"
-db.session.add(cust)
-db.session.commit()
-
-
-cust2 = Customers()
-cust2.name = "Adam"
-cust2.amount = 2035.0
-cust2.duedate = "1-2-20"
-cust2.mobileno = "9008070060"
-db.session.add(cust2)
-db.session.commit()
+with open('users.json') as f:
+    users = json.load(f)
+Users.query.delete()
+for u in users["users"]:
+    user = Users()
+    user.user = u["user"]
+    user.key = u["key"]
+    db.session.add(user)
+    db.session.commit()
+with open('customers.json') as f:
+    customers = json.load(f)
+Customers.query.delete()
+for c in customers["customers"]:
+    cust = Customers()
+    cust.name = c["customerName"]
+    cust.amount = c["dueAmount"]
+    cust.duedate = c["dueDate"]
+    cust.mobileno = c["mobileNumber"]
+    db.session.add(cust)
+    db.session.commit()
+Transactions.query.delete()
